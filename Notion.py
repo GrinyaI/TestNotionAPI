b5a0542053d2e263e1f9babf1692115070ec77f6
Ilya <grinevich2018@mail.ru>
2023-10-04 13:04:50 +0600

    Update

--- Notion.py
+++ Notion.py
@@ -15,14 +15,14 @@ def get_pages_database(DataBase_ID):
     results = data["results"]
     return results
 
-def update_page(PAGE_ID: str, NEW_DATA: dict):
-    url = f"https://api.notion.com/v1/pages{PAGE_ID}"
-
-    payload = {"properties": NEW_DATA}
-
-    res = requests.patch(url, json=payload, headers=HEADERS)
-    print(res.status_code)
-    return res
+# def update_page(PAGE_ID: str, NEW_DATA: dict):
+#     url = f"https://api.notion.com/v1/pages{PAGE_ID}"
+#
+#     payload = {"properties": NEW_DATA}
+#
+#     res = requests.patch(url, json=payload, headers=HEADERS)
+#     print(res.status_code)
+#     return res
 
 # def get_pages(Page_ID): #КБ!
 #     url = f"https://api.notion.com/v1/pages/{Page_ID}"
@@ -38,14 +38,7 @@ def update_page(PAGE_ID: str, NEW_DATA: dict):
 #     return results
 
 def fix_full_name(FULLNAME: str):
-    name_parts = FULLNAME.split()
-    for letter in name_parts[0]:
-        if letter == ".":
-            return "ФИО введенно неверно"
-    formatted_name = name_parts[0].capitalize() + " "
-    name_parts = name_parts[1].split(".")
-    formatted_name += name_parts[0].upper() + "." + name_parts[1].upper() + "."
-    return str(formatted_name.strip())
+    return FULLNAME.title()
 
 def fix_lesson(LESSON: str):
     les = LESSON.lower()
@@ -60,34 +53,40 @@ def fix_group(GROUP: str):
     return GROUP.upper()
 
 def find_Student(FULLNAME: str, GROUP: str, LESSON: dict):
-    if LESSON.get(GROUP) is None:
-        return False #"Такой группы в данном предмете не существует"
-    elif FULLNAME == "ФИО введенно неверно":
-        return False #"ФИО введенно неверно"
-    else:
-        pages = get_pages_database(LESSON[GROUP])
-        for page in pages:
-            props = page["properties"]
-            name = props["ФИО"]["title"][0]["text"]["content"]
-            if name == FULLNAME:
-                return True #f"Студент {name} найден в группе {GROUP}"
-        return False # f"Студент {FULLNAME} в группе {GROUP} не найден"
+    try:
+        if LESSON.get(GROUP) is None:
+            return False #"Такой группы в данном предмете не существует"
+        elif FULLNAME == "ФИО введенно неверно":
+            return False #"ФИО введенно неверно"
+        else:
+            for i in range(0, len(LESSON[GROUP]) - 1):
+                pages = get_pages_database(LESSON[GROUP][i])
+                for page in pages:
+                    props = page["properties"]
+                    name = props["Name"]["title"][0]["text"]["content"]
+                    if name == FULLNAME:
+                        return True #f"Студент {name} найден в группе {GROUP}"
+                return False # f"Студент {FULLNAME} в группе {GROUP} не найден"
+            return False
+    except:
+        print("Функция find_Student не сработала")
 
 def show_me_my_points(FULLNAME: str, GROUP: str, LESSON: dict):
     if find_Student(FULLNAME, GROUP, LESSON) != True:
         return False
     else:
-        pages = get_pages_database(LESSON[GROUP])
-        for page in pages:
-            props = page["properties"]
-            name = props["ФИО"]["title"][0]["text"]["content"]
-            points = props["Formula"]["formula"]["number"]
-            if name == FULLNAME:
-                return points
+        for i in range(0, len(LESSON[GROUP]) - 1):
+            pages = get_pages_database(LESSON[GROUP][i])
+            for page in pages:
+                props = page["properties"]
+                name = props["Name"]["title"][0]["text"]["content"]
+                points = props["Formula"]["formula"]["number"]
+                if name == FULLNAME:
+                    return points
+            return False
         return False
 def main():
     # lesson = fix_lesson(input("Введите предмет: "))
     # group = fix_group(input("Введите группу: "))
     # fullname = fix_full_name(input("Введите ФИО: "))
-    update_data = {"GitHub": {"url": "https://github.com/GrinyaI"}}
-    update_page("685812d7-5034-48ff-a1b4-d1ef82346372", update_data)
+    print(show_me_my_points(fix_full_name(input("ФИО: ")), "ПИН-221", PYTHON_LESSON))
